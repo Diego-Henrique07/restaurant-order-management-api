760cc384437155fc9e6020dc25bbb1ea188d29c1
Diego <hemriqued48@gmail.com>
2026-04-07 13:08:39 -0300

    Fix: remove unnecessary files / adjustments

--- restaurantApi/src/main/java/restaurantApi/RestaurantApiApplication.java
+++ restaurantApi/src/main/java/restaurantApi/RestaurantApiApplication.java
@@ -9,32 +9,5 @@ public class RestaurantApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantApiApplication.class, args);
 	}
-
 }
-//PATCH http://localhost:8080/orders/1/status
 
-//http://localhost:8080/customers {
-//  "id": 1,
-//  "name": "Diego",
-//  "phone": "123456"
-//}
-
-//http://localhost:8080/products
-//{
-//  "id": 1,
-//  "name": "Pizza",
-//  "price": 50.00
-//}
-
-//http://localhost:8080/orders
-//{
-//  "id": 1,
-//  "customerId": 1,
-//  "orderStatus": "CREATED",
-//  "totalPrice": 0,
-//  "createdAt": "2026-04-04T14:26:31.2916379"
-//}
-
-
-//git status
-//git add .
